af7de08ffd3657177e6da42012bb53f69c6783f5
sxng33.th <sangeethxramesh@gmail.com>
2025-01-01 19:13:25 +0530

    complete update user functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "real-estate-85d32.firebaseapp.com",
+  projectId: "real-estate-85d32",
+  storageBucket: "real-estate-85d32.firebasestorage.app",
+  messagingSenderId: "476515020105",
+  appId: "1:476515020105:web:bbe5150d01b4605ea4be98"
 };
 
 // Initialize Firebase
