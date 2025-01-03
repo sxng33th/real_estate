5b8a463d9a8c20c818ffe1c9b2803d4fd8c8dedf
sxng33.th <sangeethxramesh@gmail.com>
2025-01-03 14:10:02 +0530

    final changes

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { google, signin, signup } from '../controllers/auth.controller.js';
+import { google, signin, signOut, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.get("/signout", signOut);
 router.post('/google', google);
 
 
